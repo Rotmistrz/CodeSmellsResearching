eadb054988b91fc74a996e0c07541be1ba3d645f
Filip <filip.markiewicz96@gmail.com>
2021-05-20 13:06:19 +0200

    Initial commit

---
+++ src/MLCQ/ResearchingMLCQProgram.java
@@ -0,0 +1,51 @@
+package MLCQ;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class ResearchingMLCQProgram {
+    public static void main(String[] args) {
+        String filename = "./resources/MLCQCodeSmellSamples-32.csv";
+        String resultsDir = "./results";
+
+        String resultsCodeDir = resultsDir + "/code";
+        String resultsReviewsDir = resultsDir + "/csv";
+
+        try {
+            AnalyzerMLCQ analyzer = new AnalyzerMLCQ(filename, "./logs", ';');
+
+            List<CodeReview> reviews = analyzer.prepareReviews(filename);
+
+            int expectedFiles = reviews.size();
+
+            List<CodeReview> reachableReviews = analyzer.receiveCodeSamples(resultsCodeDir, reviews, 10);
+
+            int actualFiles = reachableReviews.size();
+
+            if (expectedFiles == actualFiles) {
+                System.out.println("Receiving files completed successfully!");
+            } else {
+                System.out.println("Receiving files failed. Received " + actualFiles + " of " + expectedFiles + " reviews.");
+            }
+
+            analyzer.saveCodeReviews(resultsReviewsDir, reachableReviews);
+        } catch (Exception e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
+    }
+}
